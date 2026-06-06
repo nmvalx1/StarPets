39f9d31f694113a82fefbd95cc90f0f44129d214
nmvalx1 <naumov.alex00@icloud.com>
2026-06-06 19:59:37 +0300

    initialize project

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: '.',
+  timeout: 30_000,
+  retries: process.env.CI ? 1 : 0,
+  reporter: [['list'], ['html', { open: 'never' }]],
+
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'api',
+      testMatch: /api\/tests\/.*\.spec\.ts/,
+    },
+    {
+      name: 'ui',
+      testMatch: /ui\/tests\/.*\.spec\.ts/,
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: process.env.UI_BASE_URL,
+        headless: true,
+      }
+    },
+  ],
+});
